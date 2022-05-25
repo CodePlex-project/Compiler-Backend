c5c1bfb49c951385af51d3be0dd59e1ad1898620
Muhammed Ashiq Abdul Khader <ashiqabdulkhader@gmail.com>
2022-05-25 14:17:20 +0530

    fastapi app

---
+++ app/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from compylex.compiler import Compile
+import json
+
+
+class Item(BaseModel):
+    code: str
+    lang: str
+    input: str
+    id: str
+
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/compile/")
+async def create_item(item: Item):
+    result = Compile(item.code, item.lang, item.input, item.id)
+    output = result.get_output()
+    status = result.get_status()
+    data = {
+        'output': output,
+        'status': status
+    }
+    data = json.dumps(data)
+    return data
